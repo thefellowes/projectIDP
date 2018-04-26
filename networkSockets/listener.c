75bb4515074ab61de9c29e161357df4e4df81d70
Marco <marcogeertsma@gmail.com>
2018-04-26 14:13:43 +0200

    can now extract data from talker

--- networkSockets/listener.c
+++ networkSockets/listener.c
@@ -8,6 +8,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <assert.h>
+
 #define MYPORT "1313" // the port users will be connecting to
 #define MAXBUFLEN 100
 // get sockaddr, IPv4 or IPv6:
@@ -17,6 +19,55 @@ void *get_in_addr(struct sockaddr *sa){
 	}
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
+
+char** str_split(char* a_str, const char a_delim)
+{
+    char** result    = 0;
+    size_t count     = 0;
+    char* tmp        = a_str;
+    char* last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
+
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result = malloc(sizeof(char*) * count);
+
+    if (result)
+    {
+        size_t idx  = 0;
+        char* token = strtok(a_str, delim);
+
+        while (token)
+        {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = 0;
+    }
+
+    return result;
+}
+
 int main(void){
 	int sockfd;
 	struct addrinfo hints, *servinfo, *p;
@@ -67,10 +118,21 @@ int main(void){
 	printf("listener: packet is %d bytes long\n", numbytes);
 	buf[numbytes] = '\0';
 	char x;
-	for(int i; i < MAXBUFLEN && buf[i] != '\0'; i++){
-		x = buf[i];
-		printf(x, "\n");
+	int lorem;
+	
+	char** tokenSwitch;
+	tokenSwitch = str_split(buf, ';');
+	
+	if(tokenSwitch){
+		int i;
+		for(i = 0; *(tokenSwitch + i); i++){
+			printf("%s\n", *(tokenSwitch + i));
+			free(*(tokenSwitch + i));
+		}
+		printf("\n");
+		free(tokenSwitch);
 	}
+	
 	printf("listener: packet contains \"%s\"\n", buf);
 	close(sockfd);
 	return 0;
