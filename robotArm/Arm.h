17ba63398d1d1e047c4d577f05fd79a2f9aaac94
WEG97 <e.g.werff@hotmail.com>
2018-05-17 16:28:46 +0200

    class for robotarm
    Class to control the robotarm. Needs to include missing file and test.

---
+++ robotArm/Arm.h
@@ -0,0 +1,29 @@
+#ifndef ARM_H
+#define ARM_H
+
+#include <vector>
+#include <string>
+#include "AX12A.h"
+
+class Arm {
+private:
+	AX12A* ax12a;
+	std::vector<int> servoIDs;
+	float posX;
+	float posY;
+	float headAngle;
+	int posRotation;
+	static const float posDifference;
+	static const float rotDifference;
+	bool posPossible(int x, int y);
+public:
+	Arm(AX12a &servoControl, std::vector<int> servoIDs);
+	int move(float speedX, float speedY);
+	int turn(float speed);
+	void moveTo(std::vector<std::vector<int>> path);
+	float getPosX();
+	float getPosY();
+	float getHeadAngle();
+}
+
+#endif //ARM_H
