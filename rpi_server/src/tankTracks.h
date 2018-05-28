69195ccfb78c2fb17363182bd8ac4bb3e33f0b57
WEG97 <e.g.werff@hotmail.com>
2018-05-28 15:44:22 +0200

    added classes to controll motoren
    These classes still needs to be test.
    Also the funciton 'wiringPiSetupGpio()' shoul probalby be called on a other place, because this function is also called in AX12A.cpp.

---
+++ rpi_server/src/tankTracks.h
@@ -0,0 +1,19 @@
+#ifndef TANKTRACKS_H
+#define TANKTRACKS_H
+
+#include "motor.h"
+
+class TankTracks {
+	private:
+		Motor leftMotor;
+		Motor rightMotor;
+		int speedLeftMotor;
+		int speedRightMotor;
+		int moveIsActive;
+	public:
+		TankTracks(Motor leftMotor, Motor rightMotor);
+		void move(float throttle, float direction);
+		void stop();
+};
+
+#endif //TANKTRACKS_H
