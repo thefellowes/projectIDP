25f0785d45da60a0e716dc52ac1a7f53f26e7d8d
WEG97 <e.g.werff@hotmail.com>
2018-05-29 14:15:22 +0200

    calculations are now correct

--- rpi_server/src/tankTracks.cpp
+++ rpi_server/src/tankTracks.cpp
@@ -15,50 +15,43 @@ TankTracks::TankTracks(Motor leftMotor, Motor rightMotor)
 
 //throttle and direction variable between -1.0 and 1.0
 void TankTracks::move(float throttle, float direction)
-{
-	//Calculate percentage of throttle versus direction
-	/**float total = positive(throttle) + positive(direction);
-	throttle = throttle / total;
-	direction = direction / total;
-
-	float percSpeedLeft = throttle + direction;
-	float percSpeedRight = throttle - direction;
-
-	speedLeftMotor = leftMotor.maxSpeed * percSpeedLeft;
-	speedRightMotor = rightMotor.maxSpeed * percSpeedRight;**/
-	
+{	
 	float maxSpeed = positive(throttle) > positive(direction) ? throttle : direction;
 	maxSpeed *= leftMotor.maxSpeed;
 	
 	//stop motors if both inputs zero and make turn on place if only direction != 0
 	if(throttle == 0){
-		if(direction == 0) stop();
+		if(direction == 0){
+			stop();
+			return;
+		}
 		else{
 			speedLeftMotor = direction * maxSpeed;
 			speedRightMotor = -1 * direction * maxSpeed;
 		}
-	}
+	}else{
+		//Calculate percentage of throttle versus direction
+		float total = positive(throttle) + positive(direction);
+		throttle = throttle / total;
+		direction = direction / total;
 
-	float total = positive(throttle) + positive(direction);
-	throttle = throttle / total;
-	direction = direction / total;
+		float speedY = throttle * maxSpeed;
+		float speedX = direction * maxSpeed;
 
-	float speedY = throttle * maxSpeed;
-	float speedX = direction * maxSpeed;
-
-	//calcuate speed for each motor to move in correct direction
-	if(throttle > 0 && direction > 0){
-		speedLeftMotor = speedY + speedX;
-		speedRightMotor = speedY;
-	}else if(throttle < 0 && direction < 0){
-		speedLeftMotor = speedY;
-		speedRightMotor = speedY + speedX;
-	}else if(throttle < 0 && direction > 0){
-		speedLeftMotor = speedY - speedX;
-		speedRightMotor = speedY;
-	}else {
-		speedLeftMotor = speedY;
-		speedRightMotor = speedY - speedX;
+		//calcuate speed for each motor to move in correct direction
+		if(throttle > 0 && direction > 0){
+			speedLeftMotor = speedY + speedX;
+			speedRightMotor = speedY;
+		}else if(throttle < 0 && direction < 0){
+			speedLeftMotor = -1* speedY;
+			speedRightMotor = -1 * (speedY + speedX);
+		}else if(throttle < 0 && direction > 0){
+			speedLeftMotor = speedY - speedX;
+			speedRightMotor = speedY;
+		}else {
+			speedLeftMotor = -1 * speedY;
+			speedRightMotor = -1 * (speedY - speedX);
+		}
 	}
 
 	//Check in which direction motor has to turn and set speed
