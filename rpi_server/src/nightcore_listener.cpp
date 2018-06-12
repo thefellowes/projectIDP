e965214b9f37ffd4f0c8e5de02ae9a1dde4844e4
Peter van der Velde <petervandervelde2@gmail.com>
2018-06-12 10:49:58 +0200

    Update nightcore_listener.cpp

--- rpi_server/src/nightcore_listener.cpp
+++ rpi_server/src/nightcore_listener.cpp
@@ -1,7 +1,12 @@
 #include <wiringPi.h>
+#include <cstdio>
+#include <chrono>
+#include <thread>
+
 #include "nightcore_listener.h"
 
 #define IN_PIN 25
+
 
 void nc_init() {
   wiringPiSetupGpio();
@@ -10,4 +15,17 @@ void nc_init() {
 
 int get_in_pin() {
   return digitalRead(IN_PIN);
+}
+
+int beat_t(void) {
+	nc_init();
+	while (1) {
+		if (get_in_pin() == HIGH) {
+			printf("High AF\n");
+			std::chrono::milliseconds timespan(500);
+			std::this_thread::sleep_for(timespan);
+		}
+	}
+
+	return 0;
 }
