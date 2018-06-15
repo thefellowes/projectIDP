179dec15f41c49bcdc0a27ada2fe31428b0516cc
Peter van der Velde <petervandervelde2@gmail.com>
2018-06-15 10:35:47 +0200

    Update parser.cpp

--- rpi_server/src/parser.cpp
+++ rpi_server/src/parser.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "parser.h"
 #include <string>
+#include "dbg.h"
 
 #define JOY_MIDDLE 512
 #define MAX_ROTATION 1023
@@ -16,73 +17,69 @@ struct user_input parse_input(char** input_data) {
 	int gripper, dance, lineDance, autoMove = -1;
 	bool doStop = false;
 	bool checkBattery = false;
-    if(input_data){
+	if(input_data){
 		for(int i = 0; *(input_data + i); i++){
-    		switch (*(input_data + i)[0]) {
+			switch (*(input_data + i)[0]) {
 				//Stop application
 				case 'S':
 					doStop = true;
 					break;
-    			//Forward and backward arm
-    			case 'x':
+					//Forward and backward arm
+				case 'x':
 					x = atof(*(input_data + i)+1);
 					x = ((x - JOY_MIDDLE) / JOY_MIDDLE) * -1;
 					if(x < 0.05 && x > -0.05){x=0;}
-    				//printf("x is:  %f\n", x);
-    				break;
-    			//Up and down arm
-    			case 'y':
-    				y = atof(*(input_data + i)+1);
+					break;
+					//Up and down arm
+				case 'y':
+					y = atof(*(input_data + i)+1);
 					y = (y - JOY_MIDDLE) / JOY_MIDDLE;
 					if(y < 0.05 && y > -0.05){y=0;}
-    				//printf("y is:  %f\n", y);
-    				break;
-    			//Forward and backward movement
-    			case 'a':
-    				a = atof(*(input_data + i)+1);
+					break;
+					//Forward and backward movement
+				case 'a':
+					a = atof(*(input_data + i)+1);
 					a = ((a - JOY_MIDDLE) / JOY_MIDDLE) * -1;
 					if(a < 0.05 && a > -0.05){a=0;}
-    				//printf("a is:  %f\n", a);
-    				break;
-    			//Left and right movement
-    			case 'b':
-    				b = atof(*(input_data + i)+1);
+					break;
+					//Left and right movement
+				case 'b':
+					b = atof(*(input_data + i)+1);
 					b = ((b - JOY_MIDDLE) / JOY_MIDDLE);
 					if(b < 0.05 && b > -0.05){b=0;}
-					//printf("b is:  %f\n", b);
-    				break;
-				//Rotation arm
-    			case 'r':
+					break;
+					//Rotation arm
+				case 'r':
 					rotation = (int)atof(*(input_data + i)+1);
 					rotation = rotation / MAX_ROTATION;
-    				break;
-				//Arm gripper (On/Off)
+					break;
+					//Arm gripper (On/Off)
 				case 'G':
 					gripper = (int)atof(*(input_data + i) + 1);
 					break;
-				//Dance (On/Off)
+					//Dance (On/Off)
 				case 'D':
 					dance = (int)atof(*(input_data + i) + 1);
 					break;
-				//Line-Dance (On/Off)
+					//Line-Dance (On/Off)
 				case 'L':
 					lineDance = (int)atof(*(input_data + i) + 1);
 					break;
-				//Auto move
+					//Auto move
 				case 'F':
 					autoMove = (int)atof(*(input_data + i) + 1);
 					break;
-				//Check battery
+					//Check battery
 				case 'B':
 					checkBattery = true;
 					break;
 				case 'A':
 					break;
-    			default:
-    				printf("unclassified token: %s\n", *(input_data + i));
-    		}
-    	}	
-    }
+				default:
+					log_warn("unclassified token: %s", *(input_data + i));
+			}
+		}	
+	}
 
 	parsed_input->doStop = doStop;
 	parsed_input->x = x;
@@ -95,6 +92,7 @@ struct user_input parse_input(char** input_data) {
 	parsed_input->lineDance = lineDance;
 	parsed_input->autoMove = autoMove;
 	parsed_input->checkBattery = checkBattery;
- 
-    return *parsed_input;
+
+	return *parsed_input;
 }
+
